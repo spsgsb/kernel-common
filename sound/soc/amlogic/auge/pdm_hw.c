8317474484b723e325076e423526215d233343e3
Jimmy Wahlberg <jimmywa@spotify.com>
2020-05-25 09:03:47 +0200

    Invert the clock to flip left/right

--- sound/soc/amlogic/auge/pdm_hw.c
+++ sound/soc/amlogic/auge/pdm_hw.c
@@ -113,7 +113,7 @@ void aml_pdm_ctrl(struct pdm_info *info)
 	/* must be sure that clk and pdm is enable */
 	aml_pdm_update_bits(PDM_CTRL,
 				(0x7 << 28 | 0xff << 8 | 0xff << 0),
-				/*(1 << 31) |*/
+				(1 << 31) |
 				/* invert the PDM_DCLK or not */
 				(0 << 30) |
 				/* output mode:  1: 24bits. 0: 32 bits */
